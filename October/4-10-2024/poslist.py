deaddd014028ba6a9a86aa6f519e0e7c598caa1e
jasirkonnola <jasirhassan.894315@gmail.com>
2024-10-04 12:24:32 +0530

    Add files via upload

---
+++ October/4-10-2024/poslist.py
@@ -0,0 +1,7 @@
+list = [11, -2 ,3 ,-1 ,10]
+
+for num in list:
+    if num >= 0 :
+        print(num)
+    
+    
---
+++ October/4-10-2024/squareNno.py
@@ -0,0 +1,5 @@
+num=int(input('enter the number:'))
+print(f'n number is {num}')
+for i in range(1,num+1):
+    sq = i * i
+    print(f'{i} ^ 2 : {sq}')
