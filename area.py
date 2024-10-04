102269f58bc03a52eb4ddfc066e5f34750d59e72
jasirkonnola <jasirhassan.894315@gmail.com>
2024-10-04 12:04:11 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,12 @@
+r=int(input("enter the radius of circle:"))
+pi=3.14
+radius=pi*r*r
+print(radius)
+b=int(input("enter the base of tiangle:"))
+h=int(input("enter the height of tiangle:"))
+a1=(b*h)/2
+print(a1)
+l=int(input("enter the length of rectangle:"))
+b1=int(input("enter the breath of rectangle:"))
+a2=l*b1
+print(a2)
---
+++ arith.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first element:"))
+b=int(input("Enter the first element:"))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print(sum)
+print(sub)
+print(mul)
+print(div)
