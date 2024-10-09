4fb663b6af6764c71e85fc672267ecac56bafba8
jasirkonnola <jasirhassan.894315@gmail.com>
2024-10-09 11:02:27 +0530

    Add files via upload

---
+++ October/week-2/largestnumber.py
@@ -0,0 +1,9 @@
+n1=int(input("enter first number:"))
+n2=int(input("enter second number:"))
+n3=int(input("enter third number:"))
+if (n1>n2) & (n1>n3) :
+     print('largest number is',n1)
+elif (n2>n1) & (n2>n3) :
+    print('largest number is',n2)
+else:
+    print('largest number is',sn3)
