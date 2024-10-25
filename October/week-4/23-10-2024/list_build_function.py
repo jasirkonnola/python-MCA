b994ff11f65ba110deffac52668f2c01d5bf0eb4
jasirkonnola <jasirhassan.894315@gmail.com>
2024-10-25 10:45:35 +0100

    Update list_build_function.py

--- October/week-4/23-10-2024/list_build_function.py
+++ October/week-4/23-10-2024/list_build_function.py
@@ -1,20 +1,17 @@
 numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
+numbers.sort()
+print("Sorted list:", numbers)
 min_value = min(numbers)
 print("Minimum value:", min_value)
 
 max_value = max(numbers)
 print("Maximum value:", max_value)
 
-num_to_count = int(input("Enter the number to count: "))
-count_num = numbers.count(num_to_count)
-print("Occurrences of", num_to_count, ":", count_num)
+count_num = len(numbers)
+print("Length of list : ", count_num)
 
 numbers.sort()
 print("Sorted list:", numbers)
-
 numbers.reverse()
 print("Reversed list:", numbers)
-
-total_sum = sum(numbers)
-print("Sum of all elements:", total_sum)
