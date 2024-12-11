9373a678ff3110673b31169e9de7e29243ae100a
jasirkonnola <jasirhassan.894315@gmail.com>
2024-12-11 10:48:02 +0530

    Add files via upload

---
+++ dec2024/pkg/Main.py
@@ -0,0 +1,17 @@
+from graphics.Circle import*
+from graphics.Rectangle import*
+from graphics.subpkg.Cuboid import*
+from graphics.subpkg.Spher import*
+       #Basic operations
+
+print("Circle Area=", circle_area(5))
+print("Perimeter of Circle=", circle_perimeter(5))
+
+print("Area of Rectangle=", rectangle_area(5,6))
+print("Perimeter of Circle=", rectangle_perimeter(5,6))
+
+print("Area of Cuboid=", cuboid_area(5,5,5))
+print("Perimeter of Cuboid=", cuboid_perimeter(5,5,5))
+
+print("Area of Sphere=", (5))
+print("volume of Sphere=", (5))
