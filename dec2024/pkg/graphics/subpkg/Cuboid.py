33c84f64921adb4d1176b00a0263356b629a5a4e
jasirkonnola <jasirhassan.894315@gmail.com>
2024-12-11 11:06:23 +0530

    Add files via upload

---
+++ dec2024/pkg/graphics/subpkg/Cuboid.py
@@ -0,0 +1,4 @@
+def cuboid_perimeter(length,breath,height):
+    return 4 * (length+breath+height)
+def cuboid_area(length,breath,height):
+    return 2 * (length*breath+breath*height+height*length)
---
+++ dec2024/pkg/graphics/subpkg/Spher.py
@@ -0,0 +1,6 @@
+def sphere_area(radius):
+    from math import pi
+    return 4 * pi * radius ** 2
+def sphere_volume(radius):
+    from math import pi
+    return (4/3) * pi * radius **3
